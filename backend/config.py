846c5b0462557dfa1526735cba4471ba547d5d60
Priyamakeshwari <priyadharshu242003@gmail.com>
2023-12-03 01:42:33 +0530

    Builder.py

--- backend/config.py
+++ backend/config.py
@@ -1,5 +1,52 @@
 import os
+from chromadb.config import Settings
+from langchain.document_loaders import CSVLoader, PDFMinerLoader, TextLoader, UnstructuredExcelLoader, Docx2txtLoader
+from langchain.document_loaders import UnstructuredFileLoader, UnstructuredMarkdownLoader
 
+SOURCE_DIRECTORY =  os.path.join(os.path.dirname(__file__),"source")
+PERSIST_DIRECTORY = os.path.join(os.path.dirname(__file__),"db")
+
+# Can be changed to a specific number
+INGEST_THREADS = os.cpu_count() or 8
+
+# Define the Chroma settings
+CHROMA_SETTINGS = Settings(
+    anonymized_telemetry=False,
+    is_persistent=True,
+)
+
+# Context Window and Max New Tokens
+CONTEXT_WINDOW_SIZE = 4096
+MAX_NEW_TOKENS = CONTEXT_WINDOW_SIZE  # int(CONTEXT_WINDOW_SIZE/4)
+
+#### If you get a "not enough space in the buffer" error, you should reduce the values below, start with half of the original values and keep halving the value until the error stops appearing
+
+N_GPU_LAYERS = 100  # Llama-2-70B has 83 layers
+N_BATCH = 512
+
+### From experimenting with the Llama-2-7B-Chat-GGML model on 8GB VRAM, these values work:
+# N_GPU_LAYERS = 20
+# N_BATCH = 512
+
+
+# https://python.langchain.com/en/latest/_modules/langchain/document_loaders/excel.html#UnstructuredExcelLoader
+DOCUMENT_MAP = {
+    ".txt": TextLoader,
+    ".md": UnstructuredMarkdownLoader,
+    ".py": TextLoader,
+    # ".pdf": PDFMinerLoader,
+    ".pdf": UnstructuredFileLoader,
+    ".csv": CSVLoader,
+    ".xls": UnstructuredExcelLoader,
+    ".xlsx": UnstructuredExcelLoader,
+    ".docx": Docx2txtLoader,
+    ".doc": Docx2txtLoader,
+}
+
+# Default Instructor Model
+EMBEDDING_MODEL_NAME = "hkunlp/instructor-large"
 MODEL_NAME = "TheBloke/zephyr-7B-beta-GGUF"
 MODEL_FILE = "zephyr-7b-beta.Q4_K_M.gguf"
 MODEL_PATH = os.path.join(os.path.dirname(__file__), "models")
+
+
