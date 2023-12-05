96563b7ba96f51d5733749b8bbfa944d3cb5c6e3
Kuldeep <71772185+neokd@users.noreply.github.com>
2023-12-06 01:59:26 +0530

    updates in streaming

--- backend/app.py
+++ backend/app.py
@@ -1,20 +1,31 @@
+
 from fastapi import FastAPI, status, WebSocket, File,UploadFile
 from langchain.llms import LlamaCpp
-from config import MODEL_PATH, MODEL_NAME, MODEL_FILE, DEVICE_TYPE, MAX_NEW_TOKENS, N_GPU_LAYERS
+from config import (
+    MODEL_PATH,
+    MODEL_NAME,
+    MODEL_FILE,
+    DEVICE_TYPE,
+    MAX_NEW_TOKENS,
+    N_GPU_LAYERS,
+)
 from huggingface_hub import hf_hub_download
 from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.callbacks.manager import CallbackManager
-import threading
-from schemas import ChatResponse
-from callbacks import StreamingLLMCallbackHandler, QuestionGenCallbackHandler
+
+from llama_cpp import Llama
+import copy
+from sse_starlette.sse import EventSourceResponse
+import json
 
 
 app = FastAPI()
 llm = None
 llm_chain = None
 callback_manager = None
+
 
 @app.get("/api/test")
 def test():
@@ -23,7 +34,7 @@ def test():
 
 @app.get("/api/load_model")
 async def load_model():
-    global llm, llm_chain,callback_manager
+    global llm, llm_chain, callback_manager
     model_path = hf_hub_download(
         repo_id=MODEL_NAME,
         filename=MODEL_FILE,
@@ -31,20 +42,20 @@ async def load_model():
         cache_dir=MODEL_PATH,
     )
     kwargs = {
-            "model_path": model_path,
-            "max_tokens": MAX_NEW_TOKENS,
-            "n_ctx": MAX_NEW_TOKENS,
-            "n_batch": 512,
-            "callback_manager": CallbackManager([StreamingStdOutCallbackHandler()]),
-            "verbose": False,
-            "f16_kv": True,
-            "streaming": True,
-        }
+        "model_path": model_path,
+        "max_tokens": MAX_NEW_TOKENS,
+        "n_ctx": MAX_NEW_TOKENS,
+        "n_batch": 512,
+        "callback_manager": CallbackManager([StreamingStdOutCallbackHandler()]),
+        "verbose": False,
+        "f16_kv": True,
+        "streaming": True,
+    }
     if DEVICE_TYPE.lower() == "mps":
         kwargs["n_gpu_layers"] = 1  # only for MPS devices
     if DEVICE_TYPE.lower() == "cuda":
         kwargs["n_gpu_layers"] = N_GPU_LAYERS  # set this based on your GPU
-            # Create a LlamaCpp object (language model)
+        # Create a LlamaCpp object (language model)
     llm = LlamaCpp(**kwargs)
 
     return {"message": "Model loaded", "status": status.HTTP_200_OK}
@@ -57,39 +68,113 @@ async def upload_user_files(file: UploadFile = File(...)):
     return {"message": "User files uploaded", "status": status.HTTP_200_OK}
 
 
-@app.websocket("/api/chat")
-async def stream_llm(websocket: WebSocket):
-    await websocket.accept()
-    model = LlamaCpp(
-        model_path="/Users/kuldeep/Project/NeoGPT/neogpt/models/models--TheBloke--Mistral-7B-Instruct-v0.1-GGUF/snapshots/45167a542b6fa64a14aea61a4c468bbbf9f258a8/mistral-7b-instruct-v0.1.Q4_K_M.gguf",
-        max_tokens=MAX_NEW_TOKENS,
-        streaming = True,
-        verbose=False,
-        n_ctx=MAX_NEW_TOKENS,
-        callback_manager = CallbackManager([StreamingStdOutCallbackHandler(),QuestionGenCallbackHandler(websocket),StreamingLLMCallbackHandler(websocket),]),
+# @app.websocket("/api/chat")
+# async def stream_llm(websocket: WebSocket):
+#     await websocket.accept()
+#     model = LlamaCpp(
+#         model_path="/Users/kuldeep/Project/NeoGPT/neogpt/models/models--TheBloke--Mistral-7B-Instruct-v0.1-GGUF/snapshots/45167a542b6fa64a14aea61a4c468bbbf9f258a8/mistral-7b-instruct-v0.1.Q4_K_M.gguf",
+#         max_tokens=MAX_NEW_TOKENS,
+#         streaming = True,
+#         verbose=False,
+#         n_ctx=MAX_NEW_TOKENS,
+#         callback_manager = CallbackManager([StreamingStdOutCallbackHandler(),SSECallbackHandler()]),
+#     )
+#     import asyncio
+#     while True:
+#         question = await websocket.receive_text()
+#         resp = ChatResponse(sender="you", message=question, type="stream")
+#         await websocket.send_json(resp.dict())
+#         await asyncio.sleep(0)
+
+#         print("Question: ", question)
+
+#         start_resp = ChatResponse(sender="bot", message="", type="start")
+#         await websocket.send_json(start_resp.dict())
+
+#         chain = LLMChain(
+#             llm=model,
+#             prompt = PromptTemplate.from_template("You are a helpful assistant, you will use the provided context to answer user questions.Read the given context before answering questions and think step by step. If you can not answer a user  question based on the provided context, inform the user. Do not use any other information for answering user. Initialize the conversation with a greeting if no context is provided. {question}"),
+#         )
+#         chain.invoke({
+#             "question": question
+#         })
+#         end_resp = ChatResponse(sender="bot", message="", type="end")
+#         await websocket.send_json(end_resp.dict())
+
+
+# llm = LlamaCpp(model_path="/Users/kuldeep/Project/NeoGPT/neogpt/models/models--TheBloke--Mistral-7B-Instruct-v0.1-GGUF/snapshots/45167a542b6fa64a14aea61a4c468bbbf9f258a8/mistral-7b-instruct-v0.1.Q4_K_M.gguf",)
+@app.get("/api/llama")
+async def chat_llama(request: Request):
+    stream = llm(
+        "Question: Who is the prime minister of India? Answer:",
+        max_tokens=100,
+        stop=["Q:", "\n"],
+        stream=True,
     )
-    import asyncio
-    while True:
-        question = await websocket.receive_text()
-        resp = ChatResponse(sender="you", message=question, type="stream")
-        await websocket.send_json(resp.dict())
-        await asyncio.sleep(0)
 
-        print("Question: ", question)
+    async def async_generator():
+        for item in stream:
+            yield item
 
-        start_resp = ChatResponse(sender="bot", message="", type="start")
-        await websocket.send_json(start_resp.dict())
+    async def sse_generator():
+        async for item in async_generator():
+            if await request.is_disconnected():
+                break
+            result = copy.deepcopy(item)
+            text = result["choices"][0]["text"]
+            yield {"data": text, "event": "message"}
 
-        chain = LLMChain(
-            llm=model,
-            prompt = PromptTemplate.from_template("You are a helpful assistant, you will use the provided context to answer user questions.Read the given context before answering questions and think step by step. If you can not answer a user  question based on the provided context, inform the user. Do not use any other information for answering user. Initialize the conversation with a greeting if no context is provided. {question}"),
-        )
-        chain.invoke({
-            "question": question
-        })
-        end_resp = ChatResponse(sender="bot", message="", type="end")
-        await websocket.send_json(end_resp.dict())
+    return EventSourceResponse(sse_generator())
+
+
+llm = LlamaCpp(
+    model_path="/Users/kuldeep/Project/NeoGPT/neogpt/models/models--TheBloke--Mistral-7B-Instruct-v0.1-GGUF/snapshots/45167a542b6fa64a14aea61a4c468bbbf9f258a8/mistral-7b-instruct-v0.1.Q4_K_M.gguf",
+    streaming=True,
+    verbose=False,
+    callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]),
+)
+llm_chain = LLMChain(
+    llm=llm,
+    prompt=PromptTemplate.from_template(
+        "You are a helpful assistant, you will use the provided context to answer user questions.Read the given context before answering questions and think step by step. If you can not answer a user  question based on the provided context, inform the user. Do not use any other information for answering user. Initialize the conversation with a greeting if no context is provided. {question}"
+    ),
+)
+
+from fastapi.responses import StreamingResponse
+from pydantic import BaseModel
+import json
+import copy
+import warnings
+
+
+class Query(BaseModel):
+    question: str
+
+
+@app.post("/api/stream_llm1")
+async def stream_llm(request: Request, query: Query):
+    async def sse_generator():
+        async for item in llm_chain.astream(query.question):
+            if await request.is_disconnected():
+                break
+            result = copy.deepcopy(item)
+            for chunk in result["text"].split():
+                yield json.dumps({"data": chunk, "event": "stream"}) + "\n"
+
+    return StreamingResponse(sse_generator(), media_type="text/event-stream")
+
+
+@app.post("/api/stream_llm2")
+async def stream_llm(request: Request, query: Query):
+    def generate_tokens():
+        result = llm_chain(query.question)
+        for token in result["text"]:
+            yield token
+
+    return StreamingResponse(generate_tokens(), media_type="application/json")
+
 
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run(app, reload=True)
--- backend/callbacks.py
+++ backend/callbacks.py
@@ -4,34 +4,63 @@ from uuid import UUID
 from langchain.callbacks.base import AsyncCallbackHandler, BaseCallbackHandler
 from langchain.schema.output import ChatGenerationChunk, GenerationChunk, LLMResult
 
-from schemas import ChatResponse
+# from schemas import ChatResponse
 
-class StreamingLLMCallbackHandler(AsyncCallbackHandler):
-    """Callback handler for streaming LLM responses."""
 
-    def __init__(self, websocket):
-        self.websocket = websocket
+# class StreamingLLMCallbackHandler(AsyncCallbackHandler):
+#     """Callback handler for streaming LLM responses."""
 
-    async def on_llm_new_token(self, token: str, **kwargs: Any) -> None:
-        try:
-            resp = ChatResponse(sender="bot", message=token, type="stream")
-            await self.websocket.send_json(resp.dict())
-        except Exception as e:
-            print(f"Error in on_llm_new_token: {e}")
+#     def __init__(self, websocket):
+#         self.websocket = websocket
 
-class QuestionGenCallbackHandler(AsyncCallbackHandler):
-    """Callback handler for question generation."""
+#     async def on_llm_new_token(self, token: str, **kwargs: Any) -> None:
+#         try:
+#             resp = ChatResponse(sender="bot", message=token, type="stream")
+#             await self.websocket.send_json(resp.dict())
+#         except Exception as e:
+#             print(f"Error in on_llm_new_token: {e}")
 
-    def __init__(self, websocket):
-        self.websocket = websocket
 
-    async def on_llm_start(
-        self, serialized: Dict[str, Any], prompts: List[str], **kwargs: Any
+# class QuestionGenCallbackHandler(AsyncCallbackHandler):
+#     """Callback handler for question generation."""
+
+#     def __init__(self, websocket):
+#         self.websocket = websocket
+
+#     async def on_llm_start(
+#         self, serialized: Dict[str, Any], prompts: List[str], **kwargs: Any
+#     ) -> None:
+#         try:
+#             resp = ChatResponse(
+#                 sender="bot", message="Synthesizing question...", type="info"
+#             )
+#             await self.websocket.send_json(resp.dict())
+#         except Exception as e:
+#             print(f"Error in on_llm_start: {e}")
+
+
+import asyncio
+from typing import AsyncGenerator
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+
+from queue import Queue
+import queue
+import sys
+
+q = queue.Queue()
+stop_item = "###finish###"
+
+
+class MyCustomHandler(StreamingStdOutCallbackHandler):
+    def on_llm_start(
+        self, serialized: dict[str, any], prompts: list[str], **kwargs: any
     ) -> None:
-        try:
-            resp = ChatResponse(
-                sender="bot", message="Synthesizing question...", type="info"
-            )
-            await self.websocket.send_json(resp.dict())
-        except Exception as e:
-            print(f"Error in on_llm_start: {e}")
+        """Run when LLM starts running."""
+        with q.mutex:
+            q.queue.clear()
+
+    def on_llm_new_token(self, token: str, **kwargs: any) -> None:
+        """Run on new LLM token. Only available when streaming is enabled."""
+        sys.stdout.write(token)
+        sys.stdout.flush()
+        q.put(token)
