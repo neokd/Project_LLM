d994b7447fd97c11d920b852c8650bd981880e76
Priyamakeshwari <priyadharshu242003@gmail.com>
2023-12-08 00:37:43 +0530

    streaming

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI, status, File, UploadFile
+from fastapi import FastAPI , status , UploadFile,  Depends  
+from fastapi.middleware.cors import CORSMiddleware  
+from pydantic import BaseModel
 from langchain.llms import LlamaCpp
 from config import (
     MODEL_PATH,
@@ -9,39 +11,20 @@ from config import (
     N_GPU_LAYERS,
 )
 from huggingface_hub import hf_hub_download
-from langchain.chains import LLMChain
+from langchain.chains import LLMChain , ConversationChain
 from langchain.prompts import PromptTemplate
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
-from callbacks import  ThreadedGenerator
 from langchain.callbacks.manager import CallbackManager
-from fastapi.responses import StreamingResponse
-from streaming import chat
-import copy
-import json
-from fastapi import Request
-from pydantic import BaseModel
-from sse_starlette.sse import EventSourceResponse
-from llama_cpp import Llama
-from fastapi.middleware.cors import CORSMiddleware
+from typing import List
+from callbacks import StreamingResponse, TokenStreamingCallbackHandler
 
 
 app = FastAPI()
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # You can replace "*" with your frontend's origin(s)
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 llm = None
-llm_chain = None
-callback_manager = None
 
-
-@app.get("/api/test")
-def test():
-    return {"message": "LLM Test API", "status": status.HTTP_200_OK}
-
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
 
 @app.get("/api/load_model")
 async def load_model():
@@ -63,17 +46,14 @@ async def load_model():
         "streaming": True,
     }
     
-    llm = Llama(model_path=model_path, max_tokens=MAX_NEW_TOKENS, n_ctx=MAX_NEW_TOKENS, n_batch=512,  verbose=False, f16_kv=True, streaming=True)
-    # if DEVICE_TYPE.lower() == "mps":
-    #     kwargs["n_gpu_layers"] = 1  # only for MPS devices
-    # if DEVICE_TYPE.lower() == "cuda":
-    #     kwargs["n_gpu_layers"] = N_GPU_LAYERS  # set this based on your GPU
-    #     # Create a LlamaCpp object (language model)
-    # llm = LlamaCpp(**kwargs)
+    if DEVICE_TYPE.lower() == "mps":
+        kwargs["n_gpu_layers"] = 1  # only for MPS devices
+    if DEVICE_TYPE.lower() == "cuda":
+        kwargs["n_gpu_layers"] = N_GPU_LAYERS  # set this based on your GPU
+        # Create a LlamaCpp object (language model)
+    llm = LlamaCpp(**kwargs)
 
     return {"message": "Model loaded", "status": status.HTTP_200_OK}
-
-from typing import List
 
 @app.post("/api/upload/user_files")
 async def upload_user_files(files: List[UploadFile]):
@@ -86,66 +66,46 @@ async def upload_user_files(files: List[UploadFile]):
 
     return {"file_success":[file.filename for file in files]}
 
-class Query(BaseModel):
-    question: str
+class ChatInput(BaseModel):
+    input: str
 
-# Streaming with FastAPI
-@app.post("/api/llama")
-async def chat_llama(request: Request, question: Query):
 
-    stream = llm(
-        str(question.question),
-        max_tokens=100,
-        stop=["Q:"],
-        stream=True,
+def chain_factory() -> ConversationChain: 
+    model_path = hf_hub_download(
+        repo_id=MODEL_NAME,
+        filename=MODEL_FILE,
+        resume_download=True,
+        cache_dir=MODEL_PATH,
+    )
+    kwargs = {
+        "model_path": model_path,
+        "max_tokens": MAX_NEW_TOKENS,
+        "n_ctx": MAX_NEW_TOKENS,
+        "n_batch": 512,
+        "callback_manager": CallbackManager([StreamingStdOutCallbackHandler]),
+        "verbose": False,
+        "f16_kv": True,
+        "streaming": True,
+    }
+    
+    if DEVICE_TYPE.lower() == "mps":
+        kwargs["n_gpu_layers"] = 1  # only for MPS devices
+    if DEVICE_TYPE.lower() == "cuda":
+        kwargs["n_gpu_layers"] = N_GPU_LAYERS  # set this based on your GPU
+        # Create a LlamaCpp object (language model)
+    llm = LlamaCpp(**kwargs)
+    return ConversationChain(
+        llm = llm
     )
 
-    async def async_generator():
-        for item in stream:
-            yield item
-            print(item)
-
-    async def sse_generator():
-        async for item in async_generator():
-            if await request.is_disconnected():
-                break
-            result = copy.deepcopy(item)
-            text = result["choices"][0]["text"]
-            finish_reason = result["choices"][0]["finish_reason"]
-            yield json.dumps({"id" : result["id"],"data": text, "event": "stream", "finish_reason": finish_reason})
-
-
-    return EventSourceResponse(sse_generator(),media_type='text/event-stream')
-
-# Fake Streaming with LangChain
-@app.get("/api/stream")
-async def stream_llm(request: Request):
-    global llm_chain, llm
-    llm_chain = LLMChain(
-        llm = llm,
-        prompt=PromptTemplate.from_template(
-        "You are a helpful assistant, you will use the provided context to answer user questions.Read the given context before answering questions and think step by step. If you can not answer a user  question based on the provided context, inform the user. Do not use any other information for answering user. Initialize the conversation with a greeting if no context is provided. {question}. Always generate response in markdown format.",
-        )
+@app.post("/api/chat")
+async def chat(request: ChatInput, chain: ConversationChain = Depends(chain_factory)):
+    return StreamingResponse(
+        chain=chain,
+        config={
+            "inputs": request.model_dump(),
+            "callbacks": [
+                TokenStreamingCallbackHandler(output_key=chain.output_key),
+            ],
+        },
     )
-    async def sse_generator():
-        async for item in llm_chain.astream("List top 5 places to visit in india"):
-            if await request.is_disconnected():
-                break
-            result = copy.deepcopy(item)
-            for chunk in result["text"].split():
-                yield json.dumps({"data": chunk, "event": "stream"}) + "\n"
-                
-
-    return EventSourceResponse(sse_generator(), media_type="text/event-stream")
-
-# Streaming with Threading
-@app.get("/api/model/stream")
-async def stream():
-    return StreamingResponse(chat("List top 5 places to visit in india"), media_type='text/event-stream')
-
-
-
-if __name__ == "__main__":
-    import uvicorn
-
-    uvicorn.run(app, reload=True)
