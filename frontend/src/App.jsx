941be910b424880e7502096cf45d2f76eed83343
kirupa22 <gk22vpm@gmail.com>
2023-12-13 11:43:40 +0530

    Admin Dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./components/Home"
 import Login from "./components/Login"
 import Register from "./components/Register"
+import Admin from "./components/admin/AdminDashboard"
+
 function App() {
 
   return (
@@ -10,7 +12,7 @@ function App() {
           <Route path="/" Component={Login} />
           <Route path="/register" Component={Register} />
           <Route path="/chat" Component={Home} />
-
+          <Route path="/admin" Component={Admin} />
         </Routes>
       </BrowserRouter>
   )
